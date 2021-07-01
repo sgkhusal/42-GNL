199c7575c4e66ab3ca1106361fd043c2e3bc714e
user42 <sheela.khusal.08@gmail.com>
2021-07-01 19:19:33 +0200

    Updated tests scripts

--- gnl-testers/4_run_testers.sh
+++ gnl-testers/4_run_testers.sh
@@ -1,26 +1,58 @@
 cd ~
 cd gnl # folder where it is the gnl repo from 42
 
-# CLONE ALL GITHUB TESTERS
+# CLONE AND RUN GNL_lover -------------
 git clone https://github.com/charMstr/GNL_lover.git
-git clone https://github.com/lgrellie/gnl_tester.git
-git clone https://github.com/mrjvs/42cursus_gnl_tests.git
-git clone https://github.com/Mazoise/42TESTERS-GNL.git
-git clone https://github.com/Hellio404/Get_Next_Line_Tester.git
-git clone https://github.com/C4r4c0l3/gnl-war-machine-v2019.git
-git clone https://github.com/Sherchryst/gnlkiller.git
-git clone https://github.com/harm-smits/gnl-unit-test.git
-git clone https://github.com/Tripouille/gnlTester.git
-git clone https://github.com/saarikoski-jules/gnl_unit_tests.git
-
-# RUN GNL_lover -------------
 cd GNL_lover
 cp ../get_next_line.c ../get_next_line_utils.c ../get_next_line.h copy_in_here_GNL_files
 #cp ../get_next_line_bonus.c ../get_next_line_utils_bonus.c ../get_next_line_bonus.h copy_in_here_GNL_files
 ./GNL_lover.sh
 cd ..
 
-# RUN gnl_tester
+# CLONE AND RUN Get_Next_Line_Tester --------------
+git clone https://github.com/Hellio404/Get_Next_Line_Tester.git
+cd Get_Next_Line_Tester
+bash run.sh
+cd ..
+
+# CLONE AND RUN gnlTester ----------------
+git clone https://github.com/Tripouille/gnlTester.git
+cd gnlTester
+make m
+cd ..
+
+# CLONE AND RUN 42cursus_gnl_tests-------------
+git clone https://github.com/mrjvs/42cursus_gnl_tests.git
+cd 42cursus_gnl_tests
+mkdir get_next_line
+cp ../get_next_line.c ../get_next_line_utils.c ../get_next_line.h get_next_line
+#cp ../get_next_line_bonus.c ../get_next_line_utils_bonus.c ../get_next_line_bonus.h get_next_line
+make
+cd ..
+
+# CLONE AND RUN 42TESTERS-GNL--------------
+git clone https://github.com/Mazoise/42TESTERS-GNL.git
+cd 42TESTERS-GNL
+./all_tests.sh
+cd ..
+
+# CLONE AND RUN gnl-war-machine-v2019
+git clone https://github.com/C4r4c0l3/gnl-war-machine-v2019.git
+cd gnl-war-machine-v2019
+sed -i '' "s/..\/..\/get_next_line/../" my_config.sh
+./grademe.sh
+cd ..
+
+# CLONE AND RUN gnlkiller
+git clone https://github.com/Sherchryst/gnlkiller.git
+cd gnlkiller
+cp ../get_next_line.c ../get_next_line_utils.c ../get_next_line.h .
+#cp ../get_next_line_bonus.c ../get_next_line_utils_bonus.c ../get_next_line_bonus.h .
+bash test.sh
+cd ..
+
+# CLONE AND RUN gnl_tester
+git clone https://github.com/lgrellie/gnl_tester.git
 cd gnl_tester
 cp ../get_next_line.c ../get_next_line_utils.c ../get_next_line.h .
 #cp ../get_next_line_bonus.c ../get_next_line_utils_bonus.c ../get_next_line_bonus.h .
@@ -39,50 +71,16 @@ cp ../get_next_line.c ../get_next_line_utils.c ../get_next_line.h .
 # ./run_tests.sh 9999999999
 cd ..
 
-# RUN 42cursus_gnl_tests
-cd 42cursus_gnl_tests
-mkdir get_next_line
-cp ../get_next_line.c ../get_next_line_utils.c ../get_next_line.h get_next_line
-#cp ../get_next_line_bonus.c ../get_next_line_utils_bonus.c ../get_next_line_bonus.h get_next_line
-make
-cd ..
-
-# RUN 42TESTERS-GNL
-cd 42TESTERS-GNL
-./all_tests.sh
-cd ..
-
-# RUN Get_Next_Line_Tester --------------
-cd Get_Next_Line_Tester
-bash run.sh
-cd ..
-
-# RUN gnl-war-machine-v2019 ----------------
-cd gnl-war-machine-v2019
-sed -i '' "s/..\/..\/get_next_line/../" my_config.sh
-./grademe.sh
-cd ..
-
-# RUN gnlkiller
-cd gnlkiller
-cp ../get_next_line.c ../get_next_line_utils.c ../get_next_line.h .
-#cp ../get_next_line_bonus.c ../get_next_line_utils_bonus.c ../get_next_line_bonus.h .
-bash test.sh
-cd ..
-
-# RUN gnlTester ----------------
-cd gnlTester
-make m
-cd ..
-
-# RUN gnl-unit-test
+# CLONE AND RUN gnl-unit-test
+git clone https://github.com/harm-smits/gnl-unit-test.git
 cd gnl-unit-test
 cp ../get_next_line.c ../get_next_line_utils.c ../get_next_line.h .
 #cp ../get_next_line_bonus.c ../get_next_line_utils_bonus.c ../get_next_line_bonus.h .
 bash test.sh
 cd ..
 
-# RUN gnl_unit_tests
+# CLONE AND RUN gnl_unit_tests
+git clone https://github.com/saarikoski-jules/gnl_unit_tests.git
 cd gnl_unit_tests
 sed -i '' "s/PATH_GNL=\"..\/get_next_line\"/PATH_GNL=../" run_tests.sh
 sh run_tests.sh
